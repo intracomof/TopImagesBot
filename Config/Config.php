8dd10c3ad91c73d441ad7e6bba0b8222983d123e
oleksandrpinchuk <intracomof@gmail.com>
2019-03-03 01:06:15 +0200

    removed & regenerated creds

--- Config/Config.php
+++ Config/Config.php
@@ -8,9 +8,9 @@
 
 class Config
 {
-    public static $telegramApiKey = '765893094:AAEwUKwKzpEhpL29yV-M-9feFmNO6rKjnm8';
-    public static $googleApiKey = 'AIzaSyAuvjKOBk-47yVof17uaV3lkWyVsiXTlw4';
+    public static $telegramApiKey = 'TELEGRAM_BOT_API_KEY';
+    public static $googleApiKey = 'GOOGLE_API_KEY';
     /** @var string Google Custom Search Engine ID */
-    public static $googleCx = '014892973351494446937:voa5u-wfbqo';
-    public static $bingApiKey = '431ddf65c7984baba6dbd48dfef0dac8';
+    public static $googleCx = 'GOOGLE_CX_ID';
+    public static $bingApiKey = 'BING_SUBSCRIPTION_KEY';
 }
